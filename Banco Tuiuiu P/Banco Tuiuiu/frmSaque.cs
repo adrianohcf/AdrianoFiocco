7d332715dfe836748b70a75e9acb0e7b83ea1663
adriano.hcfiocco <adriano.hcfiocco@SJR0561292W10-1>
2019-04-26 19:36:14 -0300

    primeiro commit

---
+++ Banco Tuiuiu P/Banco Tuiuiu/frmSaque.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco_Tuiuiu
+{
+    public partial class frmSaque : Form
+    {
+        private decimal saldo;
+
+        public decimal Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+        public frmSaque()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSaque_Load(object sender, EventArgs e)
+        {
+            txtSaldoSaque.Text = 
+        }
+
+        private void btnCancelarSaque_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
